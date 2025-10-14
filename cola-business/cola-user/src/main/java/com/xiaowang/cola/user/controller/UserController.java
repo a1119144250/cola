71ed609ce0a40d9a18149d8a8a957905c3be0f39
“wb_wangjin11” <wb_wangjin11@meituan.com>
2025-10-14 15:36:25 +0800

    1

--- cola-business/cola-user/src/main/java/com/xiaowang/cola/user/controller/UserController.java
+++ cola-business/cola-user/src/main/java/com/xiaowang/cola/user/controller/UserController.java
@@ -15,7 +15,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,8 +35,7 @@ import static com.xiaowang.cola.user.infrastructure.exception.UserErrorCode.USER
 @RequestMapping("user")
 public class UserController {
 
-  @Autowired
-  private UserService userService;
+  private final UserService userService;
 
   @GetMapping("/getUserInfo")
   public Result<UserInfo> getUserInfo() {
